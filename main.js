f95c8d3079104dbe1d3d0d8ae1faa5d536be3982
Seema-97 <134265455+Seema-97@users.noreply.github.com>
2023-09-28 17:59:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,182 @@
+const selectElm = document.getElementById('avatar');
+
+const btnElm = document.getElementById('js-button');
+
+const imgDivElm = document.getElementById('js-image-row');
+
+
+
+btnElm.addEventListener('click', () =>
+{
+   if (selectElm.value === 'athletic')
+   {
+    
+   //  img1Tag = document.createElement('img');
+   //  img1Tag.src = 'images/athletic.jpeg'
+   //  img1Tag.classList.add('img');
+
+   //  imgDivElm.append(img1Tag);
+
+    imgDivElm.innerHTML = `<img src="athletic.jpeg"  style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;" 
+    / >` ;
+  
+
+   }
+   else if(selectElm.value === 'cottagecore')
+   {
+    
+   imgDivElm.innerHTML = `<img src="cottagecore.jpeg" / 
+   style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"  
+    />` ;
+
+   }
+
+   else if(selectElm.value === 'monochromatic')
+   {
+
+      imgDivElm.innerHTML = `<img src="mono1.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >` ;
+
+   }
+   else if(selectElm.value === 'virbrant')
+   {
+      imgDivElm.innerHTML = `<img src="vibrant1.jpeg" 
+    style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >`;
+   }
+    
+   else if(selectElm.value === 'bussiness-casual')
+   {
+      imgDivElm.innerHTML = 
+   `
+      <img src="bussiness1.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+
+      <img src="bussiness2.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+
+      <img src="bussiness3.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+
+      <img src="bussiness4.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+
+      <img src="bussiness5.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+      ` ;
+   }
+
+     
+   else if(selectElm.value === 'classy-casual')
+   {
+      imgDivElm.innerHTML = 
+      
+   `
+      <img src="classy1.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+    / >
+
+      <img src="classy2.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+
+      <img src="classy3.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"
+      / >
+      ` ;
+   }
+
+   else if(selectElm.value === 'lumberjack')
+   {
+      imgDivElm.innerHTML = 
+      
+   `
+      <img src="lumberjack1.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px;"/ >
+
+      <img src="lumberjack2.jpeg" 
+      style="  height: 350px;
+    width:350px;
+    border-radius: 6px;
+    object-fit: cover;
+    object-position:center;
+    margin:10px auto;"/ >
+      ` ;
+   }
+
+
+}
+
+)
+
